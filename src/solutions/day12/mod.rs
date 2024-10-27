5ced3cc764504e9bd1157b3c5974ae9b60ff47b8
Marek Macznik <marek.macznik@gmail.com>
2024-10-27 23:37:33 +0100

    Add: input parsing

--- src/solutions/day12/mod.rs
+++ src/solutions/day12/mod.rs
@@ -1,4 +1,5 @@
 use crate::solver::Solver;
+use std::string::String;
 
 mod input;
 
@@ -18,9 +19,44 @@ impl Solver for Problem {
 }
 
 
+struct Moon {
+    positions: [i32; 3],
+    velocity: [i32; 3],
+}
+impl Moon {
+    fn new(pos: &Vec<i32>) -> Moon {
+        Moon {
+            positions: [ pos[0], pos[1], pos[2] ],
+            velocity: [0, 0, 0]
+        }
+    }
+}
+
+fn parse_scan_data(scan_data: &str) -> Vec<Moon> {
+    scan_data
+        .lines()
+        .skip(1)
+        .map(|moon_data| {
+            let positions: Vec<i32> = moon_data[1..moon_data.len() - 1]
+                .split(',')
+                .map(|pos_str| {
+                    pos_str.trim()[2..].parse::<i32>().unwrap()
+                })
+                .collect();
+            Moon::new(&positions)
+        })
+        .collect()
+}
+
+
 #[cfg(test)]
 mod test {
     use crate::solutions::day12::*;
+
+    #[test]
+    fn test_gravity_apply() {
+        let moons = parse_scan_data(INPUT1);
+    }
 
 //10 steps, 179 energy
 const INPUT1: &str = "
