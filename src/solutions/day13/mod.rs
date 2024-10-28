e43f0dd47df9fe44ad4dd943a107ab71b7aad9bb
Marek Macznik <marek.macznik@gmail.com>
2024-10-28 12:36:40 +0100

    Part 1 of Day 13

--- src/solutions/day13/mod.rs
+++ src/solutions/day13/mod.rs
@@ -1,5 +1,6 @@
 use crate::solver::Solver;
 use crate::intcode::*;
+use std::collections::HashSet;
 
 mod input;
 
@@ -9,11 +10,103 @@ impl Solver for Problem {
     type Ans2 = usize;
 
     fn solution1(&self) -> Self::Ans1 {
-        0
+        let mut game: Game = Game::new(input::INPUT);
+        game.run();
+        game.count_tiles(TileID::BLOCK)
     }
 
     fn solution2(&self) -> Self::Ans2 {
         0
+    }
+}
+
+#[derive(Eq, PartialEq, Hash)]
+enum TileID {
+    EMPTY,
+    WALL,
+    BLOCK,
+    PADDLE,
+    BALL
+}
+
+impl From<i64> for TileID {
+    fn from(val: i64) -> TileID {
+        match val {
+            0 => TileID::EMPTY,
+            1 => TileID::WALL,
+            2 => TileID::BLOCK,
+            3 => TileID::PADDLE,
+            4 => TileID::BALL,
+            _ => panic!("Unknown ID for tile: {}", val)
+        }
+    }
+}
+
+impl From<TileID> for i64 {
+    fn from(id: TileID) -> i64 {
+        match id {
+            TileID::EMPTY => 0,
+            TileID::WALL => 1,
+            TileID::BLOCK => 2,
+            TileID::PADDLE => 3,
+            TileID::BALL => 4,
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Hash)]
+struct Tile {
+    x: i64,
+    y: i64,
+    id: TileID
+}
+impl Tile {
+    fn new(x: i64, y: i64, id: TileID) -> Tile {
+        Tile { x: x, y: y, id: id}
+    }
+}
+
+struct Game {
+    cpu: Cpu,
+    screen: HashSet<Tile>
+}
+impl Game {
+    fn new(program: &str) -> Game {
+        Game {
+            cpu: Cpu::new(program),
+            screen: HashSet::new()
+        }
+    }
+
+    fn run(&mut self) {
+        let mut out_cnt: u8 = 0;
+        let mut x: i64 = 0;
+        let mut y: i64 = 0;
+        loop {
+            match self.cpu.step() {
+                CpuStatus::Output(out) => {
+                    match out_cnt {
+                        0 => { x = out; out_cnt += 1; },
+                        1 => { y = out; out_cnt += 1; },
+                        2 => {
+                            self.screen.insert(Tile::new(x, y, out.into()));
+                            out_cnt = 0; 
+                        },
+                        _ => panic!("Invalid out cnt: {}", out_cnt)
+                    }
+                },
+                CpuStatus::Running => continue,
+                CpuStatus::Finished => break,
+                CpuStatus::WaitForInput => unreachable!()
+            }
+        }
+    }
+
+    fn count_tiles(&self, id: TileID) -> usize {
+        self.screen
+            .iter()
+            .filter(|t| t.id == id)
+            .count()
     }
 }
 
