0a313e22b3ea843f46d0bd3f4260f4fb3333a37b
Marek Macznik <marek.macznik@gmail.com>
2024-12-02 23:37:39 +0100

    day 2, part 1

---
+++ src/solutions/day02/mod.rs
@@ -0,0 +1,62 @@
+mod input;
+use crate::solver::Solver;
+pub struct Problem;
+
+impl Solver for Problem {
+    type Ans1 = usize;
+    type Ans2 = usize;
+
+    fn solution1(&self) -> Self::Ans1 {
+        let vec = read_into_vec(input::INPUT);
+        count_safe_reports(&vec)
+    }
+
+    fn solution2(&self) -> Self::Ans2 {
+        0
+    }
+}
+
+fn read_into_vec(input: &str) -> Vec<Vec<i64>> {
+    input.lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|l| l.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn is_report_safe(report: &Vec<i64>) -> bool {
+    let inc = report.windows(2).all(|w| w[0] < w[1]);
+    let dec = report.windows(2).all(|w| w[0] > w[1]);
+    let level_diff = report.windows(2).all(|w| {
+        let diff = (w[0] - w[1]).abs();
+        diff >= 1 && diff <= 3
+    });
+    (inc || dec) && level_diff
+}
+
+fn count_safe_reports(reports: &Vec<Vec<i64>>) -> usize {
+    reports.iter()
+        .filter(|r| is_report_safe(&r))
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::day02::*;
+
+    #[test]
+    fn test_day_02_safe_reports_count() {
+        let input = read_into_vec(TEST_INPUT_1);
+        assert_eq!(2, count_safe_reports(&input));
+    }
+
+const TEST_INPUT_1: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+}
+
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,10 +1,12 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use crate::solver::Solver;
 mod day01;
+mod day02;
 
 pub fn run_solutions() {
     let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     day01::Problem {}.solve(1);
+    day02::Problem {}.solve(2);
     let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
     println!(" ******** AOC solutions time: {:#?} ******** ", end - start);
