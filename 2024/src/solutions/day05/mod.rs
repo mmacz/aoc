01836b4ff1cde1c3a0b9577c94033cad4d544515
Marek Macznik <marek.macznik@gmail.com>
2024-12-05 19:33:50 +0100

    aoc2024, day05, part 1

--- 2024/src/solutions/day05/mod.rs
+++ 2024/src/solutions/day05/mod.rs
@@ -2,17 +2,61 @@ mod input;
 use crate::solver::Solver;
 pub struct Problem;
 
+use std::collections::HashMap;
+
 impl Solver for Problem {
     type Ans1 = usize;
     type Ans2 = usize;
 
     fn solution1(&self) -> Self::Ans1 {
-        0
+        let v = split_page_ordering_and_updates(input::INPUT);
+        let valid = get_valid_updates(&v);
+        sum_middle_elements(&valid)
     }
 
     fn solution2(&self) -> Self::Ans2 {
         0
     }
+}
+
+fn get_valid_updates(input: &Vec<&str>) -> Vec<Vec<usize>> {
+    let mut rules: HashMap<usize, Vec<usize>> = HashMap::new();
+    input[0].lines()
+        .for_each(|line| {
+            let parts: Vec<&str> = line.split("|").collect();
+            let k: usize = parts[0].trim().parse().unwrap();
+            let v: usize = parts[1].trim().parse().unwrap();
+            rules.entry(k).or_insert_with(Vec::new).push(v);
+        });
+
+    input[1].lines()
+        .filter_map(|line| {
+            line.split(',')
+                .map(|num| num.trim().parse::<usize>())
+                .collect::<Result<Vec<_>, _>>()
+                .ok()
+        })
+        .filter(|v| {
+            v.windows(2).all(|window| {
+                let first = window[0];
+                let second = window[1];
+                rules.get(&first)
+                    .map_or(false, |values| values.contains(&second))
+            })
+        })
+        .collect()
+}
+
+fn sum_middle_elements(updates: &Vec<Vec<usize>>) -> usize {
+    updates.iter()
+        .map(|u| {
+            u[u.len() / 2]
+        })
+        .sum()
+}
+
+fn split_page_ordering_and_updates(input: &str) -> Vec<&str> {
+    input.split("\n\n").collect()
 }
 
 #[cfg(test)]
@@ -21,6 +65,9 @@ mod test {
 
     #[test]
     fn test_day_05_sum_ordering_rules() {
+        let v = split_page_ordering_and_updates(TEST_INPUT_1);
+        let valid = get_valid_updates(&v);
+        assert_eq!(143, sum_middle_elements(&valid));
     }
 
 const TEST_INPUT_1: &str = "47|53
