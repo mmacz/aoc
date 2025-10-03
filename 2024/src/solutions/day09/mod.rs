e80a490172056b9308733764607d3c48cd10fe4f
Marek Macznik <marek.macznik@gmail.com>
2025-10-04 01:00:59 +0200

    aoc2024, day09, part2

--- 2024/src/solutions/day09/mod.rs
+++ 2024/src/solutions/day09/mod.rs
@@ -2,7 +2,18 @@ use crate::solver::Solver;
 
 pub struct Problem;
 
-type Block = (bool, usize); // file, id
+#[derive(Clone, Copy, Debug)]
+struct Block {
+    is_file: bool,
+    id: isize,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Blob {
+    pos: usize,
+    size: usize,
+}
+
 type Filesystem = Vec<Block>;
 
 impl Solver for Problem {
@@ -15,19 +26,30 @@ impl Solver for Problem {
         checksum(&fs)
     }
 
-    fn solution2(&self, input: &str) -> Self::Ans1 {
-        0
+    fn solution2(&self, input: &str) -> Self::Ans2 {
+        let mut fs: Filesystem = build_filesystem(input);
+        rearrange_whole_blocks(&mut fs);
+        checksum(&fs)
     }
 }
 
 fn build_filesystem(input: &str) -> Filesystem {
     let mut is_file: bool = true;
-    let mut curr_id: usize = 0;
+    let mut curr_id: isize = 0;
     input
         .chars()
         .flat_map(|chr| {
-            let v: Filesystem = chr.to_digit(10)
-                .map(|n| vec![(is_file, curr_id); n as usize])
+            let v: Filesystem = chr
+                .to_digit(10)
+                .map(|n| {
+                    vec![
+                        Block {
+                            is_file,
+                            id: if is_file { curr_id } else { -1 },
+                        };
+                        n as usize
+                    ]
+                })
                 .unwrap_or_default();
             if is_file {
                 curr_id += 1;
@@ -43,12 +65,12 @@ fn rearrange_blocks(disk: &mut Filesystem) -> () {
     let mut left_idx: usize = 0;
     let mut right_idx: usize = disk.len() - 1;
 
-    while left_idx != right_idx {
+    while left_idx < right_idx {
         let l: Block = disk[left_idx];
         let r: Block = disk[right_idx];
-        match l.0 {
+        match l.is_file {
             true => left_idx += 1,
-            false => match r.0 {
+            false => match r.is_file {
                 false => right_idx -= 1,
                 true => {
                     disk[left_idx] = disk[right_idx];
@@ -59,11 +81,75 @@ fn rearrange_blocks(disk: &mut Filesystem) -> () {
     }
 }
 
+fn rearrange_whole_blocks(disk: &mut Filesystem) {
+    let mut gaps: Vec<Blob> = disk
+        .iter()
+        .enumerate()
+        .filter(|(_, block)| !block.is_file)
+        .fold(Vec::<Blob>::new(), |mut acc, (pos, _)| {
+            if let Some(last) = acc.last_mut() {
+                if last.pos + last.size == pos {
+                    last.size += 1;
+                } else {
+                    acc.push(Blob { pos, size: 1 });
+                }
+            } else {
+                acc.push(Blob { pos, size: 1 });
+            }
+            acc
+        });
+
+    let mut files: Vec<Blob> = disk
+        .iter()
+        .enumerate()
+        .filter(|(_, block)| block.is_file)
+        .fold(Vec::<Blob>::new(), |mut acc, (pos, block)| {
+            if let Some(last) = acc.last_mut() {
+                if last.pos + last.size == pos && disk[last.pos].id == block.id {
+                    last.size += 1;
+                } else {
+                    acc.push(Blob { pos, size: 1 });
+                }
+            } else {
+                acc.push(Blob { pos, size: 1 });
+            }
+            acc
+        });
+    files.reverse();
+
+    for file in files.iter_mut() {
+        if let Some((gap_idx, gap)) = gaps
+            .iter_mut()
+            .enumerate()
+            .find(|(_, gap)| gap.size >= file.size && gap.pos < file.pos)
+        {
+            for i in 0..file.size {
+                disk[gap.pos + i] = disk[file.pos + i];
+                disk[file.pos + i] = Block {
+                    is_file: false,
+                    id: -1,
+                };
+            }
+            file.pos = gap.pos - file.size;
+            gap.pos += file.size;
+            gap.size -= file.size;
+            if gap.size == 0 {
+                gaps.remove(gap_idx);
+            }
+        }
+    }
+}
+
 fn checksum(disk: &Filesystem) -> usize {
     disk.iter()
         .enumerate()
-        .filter(|(_, block)| block.0)
-        .map(|(pos, block)| pos * block.1)
+        .map(|(pos, block)| {
+            if block.is_file {
+                pos * block.id as usize
+            } else {
+                0
+            }
+        })
         .sum()
 }
 
@@ -83,6 +169,13 @@ mod tests {
         let mut fs: Filesystem = build_filesystem(TEST_INPUT_2);
         rearrange_blocks(&mut fs);
         assert_eq!(1928, checksum(&fs));
+    }
+
+    #[test]
+    fn test_day_09_filesystem_checksum_3() {
+        let mut fs: Filesystem = build_filesystem(TEST_INPUT_2);
+        rearrange_whole_blocks(&mut fs);
+        assert_eq!(2858, checksum(&fs));
     }
 
     const TEST_INPUT_1: &str = "12345";
